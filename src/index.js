9faa1b8a3a0bd88d2b3e1768f0b1b3e43934d9ce
jledbe20 <jledbe20@uncc.edu>
2023-12-11 08:54:12 -0500

    removed extraneous line in index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
 import { App } from './App';
-import reportWebVitals from './reportWebVitals';<script src="https://unpkg.com/react-router-dom/umd/react-router-dom.min.js"></script>
+import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
